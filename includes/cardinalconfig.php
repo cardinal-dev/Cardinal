6186fe9e623b34703ed101b939a04dd218aa7f0a
James McClune <falcon78921@users.noreply.github.com>
2018-01-28 21:05:48 -0500

    Update cardinalconfig.php

--- includes/cardinalconfig.php
+++ includes/cardinalconfig.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load configuration as an array. Use the actual location of your configuration file
-   $config = parse_ini_file('/home/test/cardinal_config.ini'); 
+   $config = parse_ini_file('/path/to/cardinal_config.ini'); 
    $scriptsDir = $config['scriptsdir']
 
 ?>
