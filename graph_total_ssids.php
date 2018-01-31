9917acee2bc8a61e094515297e488ccb9cfe664e
James McClune <falcon78921@users.noreply.github.com>
2018-01-30 19:27:22 -0500

    Update graph_total_ssids.php

--- graph_total_ssids.php
+++ graph_total_ssids.php
@@ -63,94 +63,15 @@ $ssids5GhzRadius = $ssids5GhzRadiusRow[0];
 
 $queryTotalSsids = $ssids24Ghz + $ssids24GhzRadius + $ssids5Ghz + $ssids5GhzRadius;
 
-// Display the amount of SSIDs registered in pie chart
-
-echo "<!doctype html>\n";
-echo "<html>\n";
-echo "\n";
-echo "<head>\n";
-echo "    <title>Total Amount of SSIDs Registered</title>\n";
-echo "    <script src=\"assets/js/Chart.bundle.js\"></script>\n";
-echo "    <script src=\"assets/js/utils.js\"></script>\n";
-echo "</head>\n";
-echo "\n";
-echo "<body>\n";
-echo "    <div id=\"canvas-holder\" style=\"width:300px\">\n";
-echo "        <canvas id=\"chart-area\" />\n";
-echo "    </div>\n";
-echo "    <script>\n";
-echo "    var randomScalingFactor = function() {\n";
-echo "        return Math.round(Math.random() * 100);\n";
-echo "    };\n";
-echo "\n";
-echo "    var config = {\n";
-echo "        type: 'pie',\n";
-echo "        data: {\n";
-echo "            datasets: [{\n";
-echo "            data: ['$queryTotalSsids'],\n";
-echo "            backgroundColor: [\n";
-echo "                \"#FF6384\",\n";
-echo "                \"#36A2EB\",\n";
-echo "                \"#FFCE56\"\n";
-echo "            ],\n";
-echo "                backgroundColor: [\n";
-echo "                    window.chartColors.yellow,\n";
-echo "                ],\n";
-echo "                label: 'Dataset 1'\n";
-echo "            }],\n";
-echo "            labels: [\n";
-echo "                \"Total Number of SSIDs Registered\",\n";
-echo "            ]\n";
-echo "        },\n";
-echo "        options: {\n";
-echo "            responsive: true\n";
-echo "        }\n";
-echo "    };\n";
-echo "\n";
-echo "    window.onload = function() {\n";
-echo "        var ctx = document.getElementById(\"chart-area\").getContext(\"2d\");\n";
-echo "        window.myPie = new Chart(ctx, config);\n";
-echo "    };\n";
-echo "\n";
-echo "    document.getElementById('randomizeData').addEventListener('click', function() {\n";
-echo "        config.data.datasets.forEach(function(dataset) {\n";
-echo "            dataset.data = dataset.data.map(function() {\n";
-echo "                return randomScalingFactor();\n";
-echo "            });\n";
-echo "        });\n";
-echo "\n";
-echo "        window.myPie.update();\n";
-echo "    });\n";
-echo "\n";
-echo "    var colorNames = Object.keys(window.chartColors);\n";
-echo "    document.getElementById('addDataset').addEventListener('click', function() {\n";
-echo "        var newDataset = {\n";
-echo "            backgroundColor: [],\n";
-echo "            data: [],\n";
-echo "            label: 'New dataset ' + config.data.datasets.length,\n";
-echo "        };\n";
-echo "\n";
-echo "        for (var index = 0; index < config.data.labels.length; ++index) {\n";
-echo "            newDataset.data.push(randomScalingFactor());\n";
-echo "\n";
-echo "            var colorName = colorNames[index % colorNames.length];;\n";
-echo "            var newColor = window.chartColors[colorName];\n";
-echo "            newDataset.backgroundColor.push(newColor);\n";
-echo "        }\n";
-echo "\n";
-echo "        config.data.datasets.push(newDataset);\n";
-echo "        window.myPie.update();\n";
-echo "    });\n";
-echo "\n";
-echo "    document.getElementById('removeDataset').addEventListener('click', function() {\n";
-echo "        config.data.datasets.splice(0, 1);\n";
-echo "        window.myPie.update();\n";
-echo "    });\n";
-echo "    </script>\n";
-echo "</body>\n";
-echo "\n";
-echo "</html>\n";
-
 $conn->close();
 
 ?>
+
+<html>
+<link rel="stylesheet" href="assets/css/metro.css">
+
+<div class="metroblock ssids left ">
+  <h1><?php echo $queryTotalSsids; ?></h1>
+  <div class="clear"></div>
+  <h2>SSIDs</h2>
+</div>
