a04464b63cb17cdf60d1965148c800252a9f1af9
James McClune <falcon78921@users.noreply.github.com>
2018-01-30 19:26:55 -0500

    Update graph_total_aps.php

--- graph_total_aps.php
+++ graph_total_aps.php
@@ -45,98 +45,20 @@ $sql="SELECT * FROM access_points";
 if ($result=mysqli_query($conn,$sql))
   {
   // Return the number of rows in result set
-  $rowcount=mysqli_num_rows($result);
+  $rowCount=mysqli_num_rows($result);
   // Free result set
   mysqli_free_result($result);
   }
 
-
-echo "<!doctype html>\n";
-echo "<html>\n";
-echo "\n";
-echo "<head>\n";
-echo "    <title>Pie Chart</title>\n";
-echo "    <script src=\"assets/js/Chart.bundle.js\"></script>\n";
-echo "    <script src=\"assets/js/utils.js\"></script>\n";
-echo "</head>\n";
-echo "\n";
-echo "<body>\n";
-echo "    <div id=\"canvas-holder\" style=\"width:300px\">\n";
-echo "        <canvas id=\"chart-area\" />\n";
-echo "    </div>\n";
-echo "    <script>\n";
-echo "    var randomScalingFactor = function() {\n";
-echo "        return Math.round(Math.random() * 100);\n";
-echo "    };\n";
-echo "\n";
-echo "    var config = {\n";
-echo "        type: 'pie',\n";
-echo "        data: {\n";
-echo "            datasets: [{\n";
-echo "            data: ['$rowcount'],\n";
-echo "            backgroundColor: [\n";
-echo "                \"#FF6384\",\n";
-echo "                \"#36A2EB\",\n";
-echo "                \"#FFCE56\"\n";
-echo "            ],\n";
-echo "                backgroundColor: [\n";
-echo "                    window.chartColors.red,\n";
-echo "                ],\n";
-echo "                label: 'Dataset 1'\n";
-echo "            }],\n";
-echo "            labels: [\n";
-echo "                \"Total Number of Access Points Registered\",\n";
-echo "            ]\n";
-echo "        },\n";
-echo "        options: {\n";
-echo "            responsive: true\n";
-echo "        }\n";
-echo "    };\n";
-echo "\n";
-echo "    window.onload = function() {\n";
-echo "        var ctx = document.getElementById(\"chart-area\").getContext(\"2d\");\n";
-echo "        window.myPie = new Chart(ctx, config);\n";
-echo "    };\n";
-echo "\n";
-echo "    document.getElementById('randomizeData').addEventListener('click', function() {\n";
-echo "        config.data.datasets.forEach(function(dataset) {\n";
-echo "            dataset.data = dataset.data.map(function() {\n";
-echo "                return randomScalingFactor();\n";
-echo "            });\n";
-echo "        });\n";
-echo "\n";
-echo "        window.myPie.update();\n";
-echo "    });\n";
-echo "\n";
-echo "    var colorNames = Object.keys(window.chartColors);\n";
-echo "    document.getElementById('addDataset').addEventListener('click', function() {\n";
-echo "        var newDataset = {\n";
-echo "            backgroundColor: [],\n";
-echo "            data: [],\n";
-echo "            label: 'New dataset ' + config.data.datasets.length,\n";
-echo "        };\n";
-echo "\n";
-echo "        for (var index = 0; index < config.data.labels.length; ++index) {\n";
-echo "            newDataset.data.push(randomScalingFactor());\n";
-echo "\n";
-echo "            var colorName = colorNames[index % colorNames.length];;\n";
-echo "            var newColor = window.chartColors[colorName];\n";
-echo "            newDataset.backgroundColor.push(newColor);\n";
-echo "        }\n";
-echo "\n";
-echo "        config.data.datasets.push(newDataset);\n";
-echo "        window.myPie.update();\n";
-echo "    });\n";
-echo "\n";
-echo "    document.getElementById('removeDataset').addEventListener('click', function() {\n";
-echo "        config.data.datasets.splice(0, 1);\n";
-echo "        window.myPie.update();\n";
-echo "    });\n";
-echo "    </script>\n";
-echo "</body>\n";
-echo "\n";
-echo "</html>\n";
-
 $conn->close();
 
 ?>
+
+<html>
+<link rel="stylesheet" href="assets/css/metro.css">
+
+<div class="metroblock accesspoint left ">
+  <h1><?php echo $rowCount; ?></h1>
+  <div class="clear"></div>
+  <h2>Access Points</h2>
+</div>
