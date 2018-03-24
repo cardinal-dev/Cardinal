89889e5a8b34d57ebea7900780f79c26ef25cd85
James McClune <falcon78921@users.noreply.github.com>
2018-03-24 12:07:18 -0400

    Update travis-builder.sh

--- scripts/install/travis-builder.sh
+++ scripts/install/travis-builder.sh
@@ -9,5 +9,12 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 
+# Install MySQL Server
+sudo apt-get purge mysql-client-core-5.6
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt install mysql-client-core-5.5
+sudo apt install mysql-server
+
 # Install Dependencies
-sudo apt-get install openssh-server openssh-client mysql-server mysql-client git apache2 python-paramiko python php7.0 php7.0-mysql snmp nmap curl traceroute whois tftpd-hpa
+sudo apt-get install openssh-server openssh-client git apache2 python-paramiko python php7.0 php7.0-mysql snmp nmap curl traceroute whois tftpd-hpa
