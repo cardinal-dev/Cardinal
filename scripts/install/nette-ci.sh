6fdc6e9e85fd40d047d63c7a733cb7aa911f1c15
James McClune <falcon78921@users.noreply.github.com>
2018-07-07 01:49:34 -0400

    Update nette-ci.sh

--- scripts/install/nette-ci.sh
+++ scripts/install/nette-ci.sh
@@ -5,4 +5,5 @@
 
 # This script is designed to test the quality of the Cardinal PHP codebase. 
 
-/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html/
+cd /var/www/html
+/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html
