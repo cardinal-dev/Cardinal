a080ae42f22bc8c67a084452ec24a4f5eb777375
James McClune <falcon78921@users.noreply.github.com>
2018-07-07 01:31:03 -0400

    ci: updated ci tests
    Signed-off-by: James McClune <jmcclune@mcclunetechnologies.net>

--- scripts/install/nette-ci.sh
+++ scripts/install/nette-ci.sh
@@ -5,8 +5,8 @@
 
 # This script is designed to test the quality of the Cardinal PHP codebase. 
 
-/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html/
-/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html/assets/templates
-/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html/includes
-/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html/includes/functions
-/root/.composer/vendor/nette/code-checker/code-checker -d /var/www/html/scripts
+/home/travis/.config/composer/vendor/nette/code-checker/code-checker -d /var/www/html/
+/home/travis/.config/composer/vendor/nette/code-checker/code-checker -d /var/www/html/assets/templates
+/home/travis/.config/composer/vendor/nette/code-checker/code-checker -d /var/www/html/includes
+/home/travis/.config/composer/vendor/nette/code-checker/code-checker -d /var/www/html/includes/functions
+/home/travis/.config/composer/vendor/nette/code-checker/code-checker -d /var/www/html/scripts
