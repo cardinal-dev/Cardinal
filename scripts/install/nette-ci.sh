0f337243e0745956bd616229fa45f96371cb7d2c
James McClune <falcon78921@users.noreply.github.com>
2018-07-07 12:17:25 -0400

    Delete nette-ci.sh

--- scripts/install/nette-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Cardinal Continuous Integration using Nette
-# falcon78921
-
-# This script is designed to test the quality of the Cardinal PHP codebase. 
-
-cd /var/www/html
-/home/travis/.config/composer/nette/code-checker/code-checker -d /var/www/html
