18ebd9d3317a3c0a783a5b7b74bce5e68f6e8d98
James McClune <jmcclune@mcclunetechnologies.net>
2019-05-26 16:59:14 +0000

    ci: updated Python3 dependencies
    Signed-off-by: James McClune <jmcclune@mcclunetechnologies.net>

--- ci/travis-builder.sh
+++ ci/travis-builder.sh
@@ -9,7 +9,8 @@ sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 
 # Install dependencies
-sudo apt-get install -y openssh-server openssh-client git apache2 python3-paramiko python3 php7.0 php7.0-mysql snmp nmap curl traceroute whois tftpd-hpa python3-setuptools python3-pip
+sudo apt-get install -y openssh-server openssh-client git apache2 php7.0 php7.0-mysql snmp nmap curl traceroute whois tftpd-hpa python3-setuptools python3-pip python3
+sudo pip3 install paramiko pyinstaller
 
 # Add Python testing
-sudo apt-get install -y python3-pyflakes
+sudo apt-get install -y pyflakes3
