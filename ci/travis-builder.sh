04f54e07c5532fcceda0be5f4f2df1403b22245b
James McClune <jmcclune@mcclunetechnologies.net>
2019-05-26 19:38:01 +0000

    ci: updated CI to fix build error

--- ci/travis-builder.sh
+++ ci/travis-builder.sh
@@ -3,13 +3,8 @@
 # Travis-CI Builder
 # falcon78921
 
-# Add ppa:ondrej/php as source for Ubuntu & update sources
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get update
-
 # Install dependencies
-sudo apt-get install -y openssh-server openssh-client git apache2 php7.0 php7.0-mysql snmp nmap curl traceroute whois tftpd-hpa python3-setuptools python3-pip python3
+sudo apt-get install -y openssh-server openssh-client git apache2 snmp nmap curl traceroute whois tftpd-hpa python3-setuptools python3-pip python3
 sudo pip3 install paramiko pyinstaller
 
 # Add Python testing
